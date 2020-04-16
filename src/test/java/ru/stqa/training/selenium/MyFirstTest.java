cd8e17ca45d5dcd07f3c77a31500d0f49154c7cb
Maria Grudinina <mary.gru@mail.ru>
2020-04-16 22:08:47 +0600

    Login test

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -3,8 +3,11 @@ package ru.stqa.training.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
 
 public class MyFirstTest {
     private WebDriver driver;
@@ -15,8 +18,13 @@ public class MyFirstTest {
     }
 
     @Test
-    public void myFirstTest() {
-        driver.get("http://google.com");
+    public void login() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        assertEquals("You are now logged in as admin",
+                driver.findElement(By.xpath("//div[@class='notice success']")).getText());
     }
 
     @After
