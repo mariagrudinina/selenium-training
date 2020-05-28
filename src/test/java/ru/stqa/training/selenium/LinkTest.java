fe4cced7b2b4b523d022060b65f539f3a4b6a7d6
Maria Grudinina <mary.gru@mail.ru>
2020-05-28 23:24:45 +0600

    Updated test to check links

--- src/test/java/ru/stqa/training/selenium/LinkTest.java
+++ src/test/java/ru/stqa/training/selenium/LinkTest.java
@@ -5,12 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.Set;
 
 
@@ -43,11 +44,10 @@ public class LinkTest {
         };
     }
 
-    public void verifyLinkOpensInNewWindow(By linkLocator, Set<String> oldWindows, String title, String mainWindow) {
-        driver.findElement(linkLocator).click();
+    public void verifyLinkOpensInNewWindow(WebElement link, Set<String> oldWindows, String mainWindow) {
+        link.click();
         String newWindow = wait.until(anyWindowOtherThan(oldWindows));
         driver.switchTo().window(newWindow);
-        wait.until(ExpectedConditions.titleIs(title));
         driver.close();
         driver.switchTo().window(mainWindow);
     }
@@ -58,21 +58,10 @@ public class LinkTest {
         driver.findElement(By.className("button")).click();
         String mainWindow = driver.getWindowHandle();
         Set<String> oldWindows = driver.getWindowHandles();
-        verifyLinkOpensInNewWindow(By.xpath("//td[text()=' (ISO 3166-1 alpha-2)']/a"),
-                oldWindows, "ISO 3166-1 alpha-2 - Wikipedia", mainWindow);
-        verifyLinkOpensInNewWindow(By.xpath("//td[text()=' (ISO 3166-1 alpha-3) ']/a"),
-                oldWindows, "ISO 3166-1 alpha-3 - Wikipedia", mainWindow);
-        verifyLinkOpensInNewWindow(By.xpath("//strong[text()='Tax ID Format']/../a"),
-                oldWindows, "Regular expression - Wikipedia", mainWindow);
-        verifyLinkOpensInNewWindow(By.xpath("//strong[text()='Address Format']/../a[.//i]"),
-                oldWindows, "International Address Format Validator: Verify Mailing Formats | Informatica",
-                mainWindow);
-        verifyLinkOpensInNewWindow(By.xpath("//strong[text()='Postcode Format']/../a"),
-                oldWindows, "Regular expression - Wikipedia", mainWindow);
-        verifyLinkOpensInNewWindow(By.xpath("//strong[text()='Currency Code']/../a"),
-                oldWindows, "List of countries and capitals with currency and language - Wikipedia", mainWindow);
-        verifyLinkOpensInNewWindow(By.xpath("//strong[text()='Phone Country Code']/../a"),
-                oldWindows, "List of country calling codes - Wikipedia", mainWindow);
+        List<WebElement> links = driver.findElements(By.xpath("//form//a[./i]"));
+        for (WebElement link : links) {
+            verifyLinkOpensInNewWindow(link, oldWindows, mainWindow);
+        }
     }
 
     @After
